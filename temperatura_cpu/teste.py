801360ac13db59cf9c4ad603a55d565690b6fa80
LucasDM19 <lucasmuzel@gmail.com>
2020-03-09 19:49:29 -0300

    ainda não funciona
    Queria pegar a temperatura da CPU.
    Não funciona em 64 bits

---
+++ temperatura_cpu/teste.py
@@ -0,0 +1,7 @@
+import wmi # pip install WMI
+w = wmi.WMI(namespace="root\OpenHardwareMonitor")
+temperature_infos = w.Sensor()
+for sensor in temperature_infos:
+    if sensor.SensorType==u'Temperature':
+        print(sensor.Name)
+        print(sensor.Value)
