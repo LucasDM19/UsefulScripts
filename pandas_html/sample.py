68206c8fd7ef98c60d7b3076dd74da87df8c9bde
LucasDM19 <lucasmuzel@gmail.com>
2020-07-16 06:16:04 -0300

    Pode filtrar, ordenar ou processar os dados depois
    And of course Pandas makes it simple to filter, sort or process the data further

--- pandas_html/sample.py
+++ pandas_html/sample.py
@@ -7,3 +7,7 @@ import pandas as pd
 calls_df, = pd.read_html("http://apps.sandiego.gov/sdfiredispatch/", header=0, parse_dates=["Call Date"])
 
 calls_df.to_csv("calls.csv", index=False)
+
+calls_df.describe()
+calls_df.groupby("Call Type").count()
+calls_df["Unit"].unique()
