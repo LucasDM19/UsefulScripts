0e7d4f3a101a7dc94983d8f86fffbfb8ec083899
LucasDM19 <lucasmuzel@gmail.com>
2020-07-16 06:14:22 -0300

    Depois de extrair o HTML, converter para json
    And how that the data lives in a DataFrame, the world is yours. Wish the data was available as json records? That’s just one more line of code!

--- pandas_html/sample.py
+++ pandas_html/sample.py
@@ -6,4 +6,4 @@ import pandas as pd
 
 calls_df, = pd.read_html("http://apps.sandiego.gov/sdfiredispatch/", header=0, parse_dates=["Call Date"])
 
-print(calls_df)
+print(calls_df.to_json(orient="records", date_format="iso"))
